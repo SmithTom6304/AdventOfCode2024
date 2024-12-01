fccfc0673caf0573d7ef79cb0c21998eb524a713
Tom <smithtom6304@gmail.com>
2024-12-01 14:51:15 +0000

    Conform day1 tests to example/result format

--- day01/tests/examples.rs
+++ day01/tests/examples.rs
@@ -4,11 +4,20 @@ use day01::ordered_list::OrderedList;
 use day01::unordered_list::UnorderedList;
 
 #[test]
-fn can_find_total_distance() {
-    let binding = fs::read_to_string("./test_input").unwrap();
+fn example() {
+        assert_eq!(11, find_distance_from_file("./test_input"))
+}
+
+#[test]
+fn result() {
+        assert_eq!(2970687, find_distance_from_file("./input"))
+}
+
+fn find_distance_from_file(path: &str) -> u32 {
+    let binding = fs::read_to_string(path).unwrap();
     let lines = binding.lines();
     let unordered_list = UnorderedList::try_from(lines).unwrap();
     let ordered_list: OrderedList = unordered_list.into();
     let total_distance: u32 = ordered_list.pairs.into_iter().map(|pair| pair.distance()).sum();
-    assert_eq!(11, total_distance)
+    total_distance
 }
